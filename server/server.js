fe35577e7d426b44f32c20dbfb60a66c5987039f
Annavarapu Rakesh <annavarapurakesh190799@gmail.com>
2026-05-22 16:12:35 +0530

    setup

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config()
+
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect(process.env.DB_URI).then(() => {
+    console.log('db connected')
+}).catch((err) => {
+    console.log(err.message)
+})
+
+
+
+const port = process.env.PORT
+app.listen(port, () => {
+    console.log(`server is running ${port}`)
+})
